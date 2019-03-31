fa225ded40db8826ef6a306ca04a2dab366c9cb8
Vagios Vlachos <bagios234@gmail.com>
2019-04-01 00:25:12 +0300

    Add ball dynamically

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    private Ball currentBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, this.transform);
+
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
