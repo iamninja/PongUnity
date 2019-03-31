29ce7aafb7e09d75fd81c70ed356a4781499f79c
Vagios Vlachos <bagios234@gmail.com>
2019-04-01 00:15:16 +0300

    Ball movement and collisions

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float speed = 1f;
+    private Rigidbody2D ballRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        ballRigidbody.velocity = new Vector2(
+            -0.5f,
+            speed
+        );
         
     }
 
@@ -14,5 +22,49 @@ public class Ball : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Bound")
+        {
+            // Collided with top bound
+            if (collider.transform.position.y > this.transform.position.y && ballRigidbody.velocity.y > 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    ballRigidbody.velocity.x,
+                    -ballRigidbody.velocity.y
+                );
+            } 
+
+            // Collided with bottom bound
+            if (collider.transform.position.y < this.transform.position.y && ballRigidbody.velocity.y < 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    ballRigidbody.velocity.x,
+                    -ballRigidbody.velocity.y
+                );
+            }
+        }
+        else if (collider.tag == "Paddle")
+        {
+            // Collided with left paddle
+            if (collider.transform.position.x < this.transform.position.x && ballRigidbody.velocity.x < 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    -ballRigidbody.velocity.x,
+                    ballRigidbody.velocity.y
+                );
+            }
+
+            // Collided with right paddle
+            if (collider.transform.position.x > this.transform.position.x && ballRigidbody.velocity.x > 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    -ballRigidbody.velocity.x,
+                    ballRigidbody.velocity.y
+                );
+            }
+        }
     }
 }
