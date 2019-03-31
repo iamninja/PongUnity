2dd47ae05fa22f9f35045f74a77e99433a823665
Vagios Vlachos <bagios234@gmail.com>
2019-03-31 20:53:34 +0300

    Add paddle script

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            0,
+            verticalMovement * speed
+        );
     }
 }
