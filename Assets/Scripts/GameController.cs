72150faf6c545695b76e703260511aa09bc56016
Vagios Vlachos <bagios234@gmail.com>
2019-04-01 22:17:08 +0300

    Scoring logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,25 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject ballPrefab;
+    public Text score1Text;
+    public Text score2Text;
+    public float scoreCoordinates = 3.42f;
 
     private Ball currentBall;
+    private int score1 = 0;
+    private int score2 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject ballInstance = Instantiate(ballPrefab, this.transform);
-
-        currentBall = ballInstance.GetComponent<Ball>();
-        currentBall.transform.position = Vector3.zero;
+        SpawnBall();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentBall != null)
+        {
+            if (currentBall.transform.position.x > scoreCoordinates)
+            {
+                score1++;
+                SpawnBall();
+            }
+            if (currentBall.transform.position.x < -scoreCoordinates)
+            {
+                score2++;
+                SpawnBall();
+            }
+        }   
+    }
+
+    private void SpawnBall()
+    {
+        if (currentBall != null)
+        {
+            Destroy(currentBall.gameObject);
+        }
+        GameObject ballInstance = Instantiate(ballPrefab, this.transform);
+
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector3.zero;
+
+        score1Text.text = score1.ToString();
+        score2Text.text = score2.ToString();
     }
 }
